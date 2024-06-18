8a71516df6f6a283203d5828e94eba4ddb607bf3
Vlad Basoon <vladbasoon@gmail.com>
2024-06-18 12:59:21 +0300

    resize done

--- main.js
+++ main.js
@@ -12,6 +12,12 @@ let cube, ground
 let pointLight, ambientLight
 
 function init() {
+  window.addEventListener('resize', () => {
+    camera.aspect = innerWidth / innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(innerWidth, innerHeight)
+  })
+
   scene = new THREE.Scene();
 
   // Renderer
